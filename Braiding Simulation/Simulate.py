7fd978bcb679054acbf1b4a4b4819d0aae55b06b
th817 <73452842+th817@users.noreply.github.com>
2021-12-15 05:17:43 +0000

    Perform Unitary Evolution Simulation by Suzuki Trotterization

--- Braiding Simulation/Simulate.py
+++ Braiding Simulation/Simulate.py
@@ -2,26 +2,21 @@ import pennylane as qml
 import Evolution
 import Initialize
 
-
-d_max, d_min = 0.99, 0.01
-tau = 1
-N = 100
-dt = tau / N
-
 dev1 = qml.device('default.qubit', wires=2)
 @qml.qnode(dev1)
-def circuit(parameter_path):
+def circuit(tau, d_min, d_max, parameter_path, system, dt, state):
     if parameter_path == 'cube':
         period_num = 6
     elif parameter_path == 'tetrahedron':
         period_num = 3
 
-    Initialize.Initialize('minus')
+    Initialize.initialize(state, d_min, d_max, system)
 
-    t = 0
-    for i in range(period_num):
-        for i in range(N):
-            # print("t: " + str(t))
-            Evolution.Evolution_(t, dt, parameter_path)
-            t = t + dt
+    for k in range(2):
+        t = 0
+        for i in range(period_num):
+            for i in range(N):
+                #print("t: " + str(t))
+                Evolution.Evolution(t, tau, d_min, d_max, parameter_path, system, dt)
+                t = t + dt
     return qml.state()
