38b2b9da0fa271279183e0a6e7e6be8656a01ec2
th817 <73452842+th817@users.noreply.github.com>
2021-12-16 10:39:42 +0000

    Initialize ground state for stenger paper

--- Braiding Simulation/Initialize.py
+++ Braiding Simulation/Initialize.py
@@ -1,11 +1,11 @@
 import pennylane as qml
 from pennylane import numpy as np
 
-def initialize(state, d_min, d_max, system):
+def initialize(state, a,d_min, d_max, system):
     if system == 'Beenakker':
         initialize_Beenakker(state, d_min, d_max)
     elif system == 'Stenger':
-        initialize_Stenger(state, d_min, d_max)
+        initialize_Stenger(state, a, d_min, d_max)
 
 
 def initialize_Beenakker(state, d_min, d_max):
@@ -24,15 +24,16 @@ def initialize_Beenakker(state, d_min, d_max):
     qml.templates.AmplitudeEmbedding(statevector, wires=[0,1], normalize=True)
 
 
-def initialize_Stenger(state, d_min, d_max):
-    d1, d2, d3 = d_min, d_min, d_max
+def initialize_Stenger(state, a, d_min, d_max):
+    d1, d2, d3 = d_max, d_min, d_min
     d = (d1**2 + d2**2 + d3**2)**(1/2)
+    D = (d1**2 + d2**2 + d3**2 + 4 * a**2)
     if state == 'plus':
-        statevector = np.array([1j * (d + d3), 0, 0, d1 + 1j * d2])
+        statevector = np.array([0, -(d1**2 + d3**2), -d2 * d3 - 1j * d1 * d, d1 * d2 - 1j * d3 * d])
         statevector = statevector / np.linalg.norm(statevector)
         print("Initial State is: " + str(statevector))
     elif state == 'minus':
-        statevector = np.array([0, 1j * (d - d3), d1 + 1j * d2, 0])
+        statevector = np.array([1j * d1, 1j * d3, 1j * d2, 2 * a + D])
         statevector = statevector / np.linalg.norm(statevector)
         print("Initial State is: " + str(statevector))
     else:
