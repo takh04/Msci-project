039a8613f64401edb7df03de491338f635744379
th817 <73452842+th817@users.noreply.github.com>
2021-12-17 10:14:52 +0000

    minor changeS

--- Braiding Simulation/Initialize.py
+++ Braiding Simulation/Initialize.py
@@ -22,7 +22,7 @@ def initialize_Beenakker():
     elif initial_state == 'odd':
         statevector = np.array([0, 1j * (d - d3), d1 + 1j * d2, 0])
         statevector = statevector / np.linalg.norm(statevector)
-        print("Initial State is: " + str(np.reshape(statevector, (4, 1))))
+        print("Initial State is: \n" + str(np.reshape(statevector, (4, 1))))
     else:
         print("Non-valid initial state")
     qml.templates.AmplitudeEmbedding(statevector, wires=[0,1], normalize=True)
@@ -35,11 +35,11 @@ def initialize_Stenger():
     if initial_state == 'even':
         statevector = np.array([0, -(d1**2 + d3**2), -d2 * d3 - 1j * d1 * d, d1 * d2 - 1j * d3 * d])
         statevector = statevector / np.linalg.norm(statevector)
-        print("Initial State is: " + str(np.reshape(statevector, (4, 1))))
+        print("Initial State is: \n" + str(np.reshape(statevector, (4, 1))))
     elif initial_state == 'odd':
         statevector = np.array([1j * d1, 1j * d3, 1j * d2, 2 * a + D])
         statevector = statevector / np.linalg.norm(statevector)
-        print("Initial State is: " + str(np.reshape(statevector, (4, 1))))
+        print("Initial State is: \n" + str(np.reshape(statevector, (4, 1))))
     else:
         print("Non-valid initial state")
     qml.templates.AmplitudeEmbedding(statevector, wires=[0,1], normalize=True)
