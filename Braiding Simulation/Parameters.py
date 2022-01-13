4e8d8bfda0c6a6274bec5fb49748009d68302b1b
th817 <73452842+th817@users.noreply.github.com>
2022-01-13 22:55:00 +0000

    added module description

--- Braiding Simulation/Parameters.py
+++ Braiding Simulation/Parameters.py
@@ -1,9 +1,19 @@
-# This module contains the parameters
+# This module contains the parameters used during the simulation
+"""
+This module contains the parameters used during the simulation
+d_max, d_min : maximum / minimum coupling strength between MZMs of different wires
+a : coupling strength between MZMs of same wires
+tau : time to execute a single step of Braiding
+N : number of discrete steps for evolving a single step of Braiding (evolving tau)
+parameter_path : path taken in a parameter space (d1, d2, d3) during evolution
+system: system (Hamiltonian) to simulate
+initial_state: initially prepared state. usually even or odd parity ground state of the Hamiltonian.
+"""
 
 d_max, d_min = 0.99, 0.01
 a = 3
 tau = 1
 N = 10
-parameter_path = 'tetrahedron'
+parameter_path = 'cube'
 system = 'Beenakker'
-initial_state = 'even'
+initial_state = 'even + odd'
