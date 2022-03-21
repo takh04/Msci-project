4c8a3cd6e2fc8178d1476ff446de45749e533c2a
th817 <73452842+th817@users.noreply.github.com>
2022-03-21 18:07:19 +0000

    Small (negligible) evolution is ignored when running with real device

---
+++ Qiskit/Braiding/Evolution.py
@@ -0,0 +1,118 @@
+"""
+This module implements circuit level functions for Quantum Hamiltonian Simulation.
+The code is adapted from Davit Khachatryan, the detailed explanation and code implementations
+can be found in https://github.com/DavitKhach/quantum-algorithms-tutorials/blob/master/Hamiltonian_simulation.ipynb
+"""
+
+from qiskit import QuantumCircuit, QuantumRegister
+import numpy as np
+
+
+def exp_all_z(circuit, quantum_register, pauli_idexes, control_qubit=None, t=1):
+    """
+    The implementation of exp(iZZ..Z t), where Z is
+    the Pauli Z operator, t is a parameter.
+    :param circuit: QuantumCircuit.
+    :param quantum_register: QuantumRegister.
+    :param pauli_idexes: the indexes from quantum_register that
+                         correspond to entries not equal to I:
+                         e.g. if we have XIYZI then the
+                         pauli_idexes = [0,2,3].
+    :param control_qubit: the control Qubit from QuantumRegister
+                          other than quantum_register.
+    :param t: the parameter t in exp(iZZ..Z t).
+    """
+    if len(pauli_idexes) == 0 and control_qubit is not None:
+        circuit.add_register(control_qubit.register)
+        circuit.u1(t, control_qubit)
+        return
+
+    # the first CNOTs
+    for i in range(len(pauli_idexes) - 1):
+        circuit.cx(quantum_register[pauli_idexes[i]],
+                   quantum_register[pauli_idexes[i + 1]])
+
+    # RZ / Controlled RZ gate
+    if control_qubit is None:
+        circuit.rz(-2 * t, quantum_register[pauli_idexes[-1]])
+    else:
+        circuit.add_register(control_qubit.register)
+        circuit.crz(-2 * t, control_qubit, quantum_register[pauli_idexes[-1]])
+
+    # the second CNOTs
+    for i in reversed(range(len(pauli_idexes) - 1)):
+        circuit.cx(quantum_register[pauli_idexes[i]], pauli_idexes[i + 1])
+
+
+
+def exp_pauli(pauli, quantum_register, control_qubit=None, t=1):
+    """
+    The circuit for the exp(i P t), where P is the Pauli term,
+    t is the parameter.
+    :param pauli: the string for the Pauli term: e.g. "XIXY".
+    :param quantum_register: QuantumRegister.
+    :param control_qubit: the control Qubit from QuantumRegister
+                          other than quantum_register.
+    :param t: the parameter t in exp(i P t).
+    :return: QuantumCircuit that implements exp(i P t) or
+             control version of it.
+    """
+    if len(pauli) != len(quantum_register):
+        raise Exception("Pauli string doesn't match to the quantum register")
+
+    pauli_circuit = QuantumCircuit(quantum_register)
+    circuit_bracket = QuantumCircuit(quantum_register)  # {I, H, H_y Blocks}
+    pauli_idexes = []
+
+    for i in range(len(quantum_register)):
+        if pauli[i] == 'I':
+            continue
+        elif pauli[i] == 'Z':
+            pauli_idexes.append(i)
+        elif pauli[i] == 'X':
+            circuit_bracket.h(quantum_register[i])
+            pauli_idexes.append(i)
+        elif pauli[i] == 'Y':
+            circuit_bracket.u2(np.pi / 2, np.pi / 2, quantum_register[i])
+            pauli_idexes.append(i)
+
+    pauli_circuit += circuit_bracket
+    exp_all_z(pauli_circuit, quantum_register, pauli_idexes, control_qubit, t)
+    pauli_circuit += circuit_bracket
+    return pauli_circuit
+
+
+def hamiltonian_simulation(hamiltonian, quantum_register=None, control_qubit=None,
+                           t=1, trotter_number=1):
+    """
+    The implementation of exp(-iHt), where H is the Hamiltonian
+    operator, t is the parameter.
+    :param hamiltonian: dictionary of Pauli terms with their weights:
+                        e.g. {"XZX": 2, "ZYI": 5, "IYZ": 7}.
+    :param quantum_register: QuantumRegister.
+    :param control_qubit: the control Qubit from QuantumRegister
+                          other than quantum_register.
+    :param t: the parameter t in exp(-iHt).
+    :param trotter_number: the Trotter number.
+    :return: QuantumCircuit that corresponds to exp(iHt)
+             or control version of it.
+    """
+
+    if quantum_register is None:
+        quantum_register = QuantumRegister(len(list(hamiltonian.keys())[0]))
+    if control_qubit in quantum_register:
+        raise Exception("the control qubit is in the target register")
+
+    delta_t = -t / trotter_number
+    exp_hamiltonian = QuantumCircuit(quantum_register)
+    exp_delta_t = QuantumCircuit(quantum_register)
+
+    for pauli in hamiltonian:
+        weight = hamiltonian[pauli]
+        if np.abs(weight * delta_t) > 10**(-6):
+            exp_delta_t += exp_pauli(pauli, quantum_register, control_qubit, weight * delta_t)
+
+    for i in range(trotter_number):
+        exp_hamiltonian += exp_delta_t
+
+    return exp_hamiltonian
