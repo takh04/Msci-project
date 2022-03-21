2d0f68d2b5c9211fbbd9a27d71af7ba71363e3f7
th817 <73452842+th817@users.noreply.github.com>
2022-03-21 18:20:13 +0000

    Simulation type and using real backend is added

---
+++ Qiskit/Braiding/Parameters.py
@@ -0,0 +1,27 @@
+# This module contains the parameters used during the simulation
+"""
+This module contains the parameters used during the simulation
+d_max, d_min : maximum / minimum coupling strength between MZMs of different wires
+a : coupling strength between MZMs of same wires
+parameter_path : path taken in a parameter space (d1, d2, d3) during evolution
+system: system (Hamiltonian) to simulate
+initial_state: initially prepared state. usually even or odd parity ground state of the Hamiltonian.
+loops = number of loops taken in the parameter space
+"""
+# Simulation Type
+real_device = False
+phase_estimation = False
+statevector = True
+proj_meas = False
+
+
+# Simulation Parameters
+d_min = 0.0001
+d_max = 0.9999
+a = 1
+parameter_path = 'cube'
+system = '6MZM'
+initial_state = 'odd g'
+loops = 1
+
+
