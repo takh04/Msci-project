d263f99e7a1a141254337002475744563be40174
th817 <73452842+th817@users.noreply.github.com>
2022-04-01 15:42:43 +0100

    even + odd added for 4MZM

--- Qiskit/Braiding/Initialize.py
+++ Qiskit/Braiding/Initialize.py
@@ -10,6 +10,10 @@ import numpy as np
 a, d_min, d_max = Parameters.a, Parameters.d_min, Parameters.d_max
 system, initial_state = Parameters.system, Parameters.initial_state
 
+"""
+This part is an initialization schemes for statevector calculation.
+Exact desired ground state is intializied without considering the circuit depth.
+"""
 def initialize(circuit, qubit):
     if system == '4MZM':
         initialize_4MZM(circuit, qubit)
@@ -32,6 +36,11 @@ def initialize_4MZM(circuit, qubit):
     state_odd_e1 = np.array([-1j * (d3 + d), d1 + 1j * d2])
     state_odd_e1 = state_odd_e1 / np.linalg.norm(state_odd_e1)
 
+    fullstate_even_g = np.array([1j * (-d3 + d), 0, 0, d1 - 1j * d2])
+    fullstate_odd_g = np.array([0, d1 + 1j * d2, 1j * (-d3 + d), 0])
+    fullstate_even_g = fullstate_even_g / np.linalg.norm(fullstate_even_g)
+    fullstate_odd_g = fullstate_odd_g / np.linalg.norm(fullstate_odd_g)
+    fullstate = (fullstate_even_g + fullstate_odd_g) / np.sqrt(2)
 
     if initial_state == 'even g':
         statevector = state_even_g
@@ -41,6 +50,8 @@ def initialize_4MZM(circuit, qubit):
         statevector = state_even_e1
     elif initial_state == 'odd e1':
         statevector = state_odd_e1
+    elif initial_state == 'even + odd':
+        statevector = fullstate
 
     print("Initial State is: \n" + str(np.reshape(statevector, (2, 1))))
     circuit.initialize(statevector, qubit)
@@ -71,14 +82,11 @@ def initialize_6MZM(circuit, qubit):
     state_odd_e2 = state_odd_e2 / np.linalg.norm(state_odd_e2)
     state_odd_e3 = state_odd_e3 / np.linalg.norm(state_odd_e3)
 
-    amplitudes = Parameters.amplitudes
     fullstate_even_g = np.array([0, 0, 0, d1 * d2 - 1j * d3 * d, 0, d2 * d3 + 1j * d1 * d, -d1 ** 2 - d3 ** 2, 0])
     fullstate_odd_g = np.array([0, 1j * d2, -1j * d3, 0, 1j * d1, 0, 0, 2 + D])
     fullstate_even_g = fullstate_even_g / np.linalg.norm(fullstate_even_g)
     fullstate_odd_g = fullstate_odd_g / np.linalg.norm(fullstate_odd_g)
-    fullstate = amplitudes[0] * fullstate_even_g + amplitudes[1] * fullstate_odd_g
-    fullstate = fullstate / np.linalg.norm(fullstate)
-
+    fullstate = (fullstate_even_g + fullstate_odd_g) / np.sqrt(2)
 
     if initial_state == 'even g':
         statevector = state_even_g
@@ -101,6 +109,83 @@ def initialize_6MZM(circuit, qubit):
 
     print("Initial State is: \n" + str(np.reshape(statevector, (len(statevector), 1))))
     circuit.initialize(statevector, qubit)
+
+
+
+"""
+This is an initialization scheme for the projective measurement.
+The purpose of this is to do initializiation / reverse initialization for real quantum device.
+The initial state may be approximated to reduce the quantum circuit depth.
+Note: Current code is incomplete. Need to be fixed."""
+def initialize_6MZM_proj(circuit, init_state, reverse=False):
+
+    if init_state == 'even + odd':
+        if reverse == False:
+            circuit.h(0)
+            circuit.x(1)
+            circuit.x(0)
+            circuit.crx(np.pi / 2, 0, 2)
+            circuit.x(0)
+            circuit.crx(-np.pi / 2, 0, 2)
+            circuit.barrier()
+        else:
+            circuit.barrier()
+            circuit.crx(np.pi / 2, 0, 2)
+            circuit.x(0)
+            circuit.crx(-np.pi / 2, 0, 2)
+            circuit.x(0)
+            circuit.x(1)
+            circuit.h(0)
+
+    elif initial_state == 'even - odd':
+        if reverse == False:
+            circuit.x(0)
+            circuit.h(0)
+            circuit.x(1)
+            circuit.x(0)
+            circuit.crx(-np.pi / 2, 0, 2)
+            circuit.x(0)
+            circuit.crx(np.pi / 2, 0, 2)
+            circuit.barrier()
+        else:
+            circuit.barrier()
+            circuit.crx(-np.pi / 2, 0, 2)
+            circuit.x(0)
+            circuit.crx(np.pi / 2, 0, 2)
+            circuit.x(0)
+            circuit.x(1)
+            circuit.h(0)
+            circuit.x(0)
+
+    elif initial_state == 'even + iodd':
+        if reverse == False:
+            circuit.rx(np.pi / 2, 0)
+            circuit.x(1)
+            circuit.x(2)
+            circuit.h(2)
+            circuit.barrier()
+        else:
+            circuit.barrier()
+            circuit.h(2)
+            circuit.x(2)
+            circuit.x(1)
+            circuit.rx(-np.pi / 2, 0)
+
+    elif initial_state == 'even - iodd':
+        if reverse == False:
+            circuit.rx(-np.pi / 2, 0)
+            circuit.x(1)
+            circuit.h(2)
+            circuit.barrier()
+        else:
+            circuit.barrier()
+            circuit.h(2)
+            circuit.x(1)
+            circuit.rx(np.pi / 2, 0)
+
+
+
+
 
 
 
